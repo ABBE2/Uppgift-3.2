4371acb667d418beb6c8cb329df140446ec96c16
Admin <Admin@TEB127030>
2023-02-10 08:55:19 +0100

    Add project files.

---
+++ Uppgift 3.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace uppgift3_2
+{
+    class uppgift3_2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Har du ¨gått ut gymnasiet?");
+            Console.WriteLine("J=ja  N=nej");
+            string x = Console.ReadLine();
+
+            Console.WriteLine("Hur gammal är du?");
+            int ålder = Convert.ToInt32(Console.ReadLine());
+
+            if (ålder >22 || x == "n")
+                    {
+                Console.WriteLine("Vi letar tyvärr efter annan personal just nu.");
+            }
+            else
+            {
+                Console.WriteLine("Vi vill gärna anställa dig.");
+            }
+        }
+    }
+}
